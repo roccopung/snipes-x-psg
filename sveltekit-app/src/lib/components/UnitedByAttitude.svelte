e66944a6d1bce78c0e58972921465a5c87bfce48
Rocco Punghellini <rocco.punghellini@gmail.com>
2026-06-02 10:52:47 +0200

    tweaks part-2

--- sveltekit-app/src/lib/components/UnitedByAttitude.svelte
+++ sveltekit-app/src/lib/components/UnitedByAttitude.svelte
@@ -7,21 +7,26 @@
 
   const { data }: Props = $props();
   let campaignHeight: number = $state(0);
-  $effect(() => {
-    console.log(campaignHeight);
-  });
+
+  const goToLooks = () => {
+    const looksSection = document.querySelector(".looks-section");
+    if (looksSection) {
+      looksSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 </script>
 
 <section
   class="united-by-attitude-section min-h-[100dvh] pt-12 pb-6 md:pt-0 flex flex-col items-start md:w-[80vw] mx-auto"
-  style:--campaign-mt="calc(50dvh - {campaignHeight / 2}px)"
 >
-  <div class="flex gap-12 flex-col">
+  <div
+    class="campaign-wrapper flex gap-12 flex-col justify-center h-auto md:h-[100svh]"
+    bind:clientHeight={campaignHeight}
+  >
     <h3 class="text-center typo-base uppercase">United By Attitude</h3>
 
     <div
       class="campaign-grid flex flex-col gap-2 px-2 md:grid md:grid-cols-4 md:py-0 md:px-0"
-      bind:clientHeight={campaignHeight}
     >
       {#if data?.campaign?.length > 0}
         {#each data?.campaign as item, i}
@@ -36,10 +41,16 @@
         {/each}
       {/if}
     </div>
-    <h3 class="text-center typo-base uppercase">Shop the look</h3>
+    <button
+      onclick={goToLooks}
+      class="text-center typo-base uppercase hover:opacity-50"
+      >Shop the looks</button
+    >
   </div>
 
-  <div class="flex flex-col gap-2 px-2 md:grid md:grid-cols-4 md:px-0 pt-12">
+  <div
+    class="flex flex-col gap-2 px-2 py-8 md:grid md:grid-cols-6 md:px-0 md:py-20 looks-section"
+  >
     {#if data?.products?.length > 0}
       {#each data?.products as item, i}
         <a
@@ -56,9 +67,9 @@
 </section>
 
 <style>
-  @media (min-width: 768px) {
-    .united-by-attitude-section {
+  /*@media (min-width: 768px) {
+    .campaign-wrapper {
       padding-top: var(--campaign-mt);
     }
-  }
+  }*/
 </style>
